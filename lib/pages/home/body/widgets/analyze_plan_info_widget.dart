5fc7981a84e42bb49e0b7fb9282e6059167e7ee3
kthdd1234 <kthdd12@daum.net>
2023-05-19 08:11:45 +0900

    import 빈 공백 제거하고 다시 커밋

--- lib/pages/home/body/widgets/analyze_plan_info_widget.dart
+++ lib/pages/home/body/widgets/analyze_plan_info_widget.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_app_weight_management/components/%08segmented/default_segmented.dart';
+import 'package:flutter_app_weight_management/components/segmented/default_segmented.dart';
 import 'package:flutter_app_weight_management/components/chart/default_chart.dart';
 import 'package:flutter_app_weight_management/components/contents_box/contents_box.dart';
 import 'package:flutter_app_weight_management/components/divider/width_divider.dart';
